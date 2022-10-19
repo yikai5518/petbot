1b17895219bb75c47c9223beabfac321b312fb64
IPAMSTER <yikaifan@umich.edu>
2022-10-19 15:43:39 -0400

    Add functions for data preprocessing

--- petbot/dataset.py
+++ petbot/dataset.py
@@ -1,13 +1,24 @@
 from pathlib import Path
+from typing import Tuple
 import pandas as pd
 from flair.datasets import ColumnCorpus, ClassificationCorpus
 
 
-def transform_ner_data(path: str | Path) -> None:
+def clean(text : str) -> str:
+    filters = ["!", "#", "$", "%", "&", "(", ")", "/", "*", ".", ":", ";", "<", "=", ">", "?", "@", "[",
+               "\\", "]", "_", "`", "{", "}", "~", "'"]
+    for i in text:
+        if i in filters:
+            text = text.replace(i, " " + i)
+            
+    return text
+
+
+def transform_ner_data(df: pd.DataFrame, path: str | Path) -> None:
     pass
 
 
-def transform_intent_data(path: str | Path) -> None:
+def transform_intent_data(df: pd.DataFrame, path: str | Path) -> None:
     return
 
 
@@ -29,3 +40,19 @@ def load_intent_data(path: str | Path) -> ClassificationCorpus:
         test_file="test.txt",
         dev_file="dev.txt",
     )
+
+
+def load_data(path: str | Path) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    pass
+
+
+if __name__ == "__main__":
+    train_data, test_data, dev_data = load_data("data/conversation.csv")
+    
+    transform_intent_data(train_data, "data/intent/train.txt")
+    transform_intent_data(test_data, "data/intent/test.txt")
+    transform_intent_data(dev_data, "data/intent/dev.txt")
+    
+    transform_ner_data(train_data, "data/ner/train.txt")
+    transform_ner_data(test_data, "data/ner/test.txt")
+    transform_ner_data(dev_data, "data/ner/dev.txt")
